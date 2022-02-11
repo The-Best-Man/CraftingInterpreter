cbb4be2a5fdb04d8deb2992a5fa0e0d11f48be82
The-Best-Man <super1999ipk@gmail.com>
2022-02-10 19:50:02 -0500

    first commit for chapter 4

---
+++ lox/Lox.java
@@ -0,0 +1,82 @@
+package programs.craftInt.chapter4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox
+{
+	static boolean hadError = false;
+
+	public static void main(String[] args) throws IOException
+	{
+		// Only allow the path of the file to execute
+		if (args.length > 1)
+		{
+			System.out.println("Usage: jlox [script]");
+			System.exit(64);
+		}
+		else if (args.length == 1)
+		{ // A file was inputted
+			runFile(args[0]);
+		}
+		else
+		{ // Run the active interpreter
+			runPrompt();
+		}
+	}
+
+
+
+	public static void runFile(String path) throws IOException
+	{
+		byte[] bytes = Files.readAllBytes(Paths.get(path));
+		run(new String(bytes, Charset.defaultCharset()));
+	
+		if(hadError) System.exit(65);
+	}
+
+	public static void runPrompt() throws IOException
+	{
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input);
+
+		for (;;)
+		{
+			System.out.print("> ");
+			String line = reader.readLine();
+			if (line == null) break;
+			run(line);
+
+			hadError = false;
+		}
+	}
+
+	private static void run(String source)
+	{
+		Scanner scanner = new Scanner(source);
+		List<Token> tokens = scanner.scanTokens();
+
+		// Just print the tokens
+		for (Token token : tokens)
+		{
+			System.out.println(token);
+		}
+	}
+
+	static void error(int line, String message)
+	{
+		report(line, "", message);
+	}
+
+	private static void report(int line, String where, String message)
+	{
+		System.err.println("[line " + line + "] Error" + where + ": " + message);
+		hadError = true;
+	}
+}
+
---
+++ lox/Scanner.java
@@ -0,0 +1,263 @@
+package programs.craftInt.chapter4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static programs.craftInt.chapter4.TokenType.*;
+
+class Scanner
+{
+	private final String source;
+	private final List<Token> tokens = new ArrayList<>();
+	private int start = 0;
+	private int current = 0;
+	private int line = 1;
+	
+	private static final Map<String, TokenType> keywords;
+
+	static
+	{
+		keywords = new HashMap<>();
+		keywords.put("and",    AND);
+		keywords.put("class",  CLASS);
+		keywords.put("else",   ELSE);
+		keywords.put("false",  FALSE);
+		keywords.put("for",    FOR);
+		keywords.put("fun",    FUN);
+		keywords.put("if",     IF);
+		keywords.put("nil",    NIL);
+		keywords.put("or",     OR);
+		keywords.put("print",  PRINT);
+		keywords.put("return", RETURN);
+		keywords.put("super",  SUPER);
+		keywords.put("this",   THIS);
+		keywords.put("true",   TRUE);
+		keywords.put("var",    VAR);
+		keywords.put("while",  WHILE);
+	}
+
+	Scanner(String source)
+	{
+		this.source = source;
+	}
+
+	List<Token> scanTokens()
+	{
+		while (!isAtEnd())
+		{
+			// At the beginning of the lexeme.
+			start = current;
+			scanToken();
+		}
+
+		tokens.add(new Token(EOF, "", null, line));
+		return tokens;
+	}
+
+	private boolean isAtEnd()
+	{
+		return current >= source.length();
+	}
+
+	private void scanToken()
+	{
+		// Handle the single character lexemes first
+		char c = advance();
+		switch(c)
+		{
+			case '(': addToken(LEFT_PAREN); break;
+			case ')': addToken(RIGHT_PAREN); break;
+			case '{': addToken(LEFT_BRACE); break;
+			case '}': addToken(RIGHT_BRACE); break;
+			case ',': addToken(COMMA); break;
+			case '.': addToken(DOT); break;
+			case '-': addToken(MINUS); break;
+			case '+': addToken(PLUS); break;
+			case ';': addToken(SEMICOLON); break;
+			case '*': addToken(STAR); break;
+			case '!':
+				  addToken(match('=') ? BANG_EQUAL : BANG);
+				  break;
+			case '=':
+				  addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+				  break;
+			case '<':
+				  addToken(match('=') ? LESS_EQUAL : LESS);
+				  break;
+			case '>':
+				  addToken(match('=') ? GREATER_EQUAL : GREATER);
+				  break;
+			case '/':
+				  if (match('/'))
+				  {
+					  // A comment goes until the end of a line.
+					  while (peek() != '\n' && !isAtEnd()) advance();
+				  }
+				  else if (match('*'))
+				  {	 
+					  int counter = 0;
+					  // Advance the stream until either a closing comment or the end of the line
+					  while ((peek() != '*' || peekNext() != '/') && !isAtEnd()) 
+					  {
+						  advance();
+					  }
+					  advance();
+					  advance();
+				  }
+				  else
+				  {
+					  addToken(SLASH);
+				  }
+				  break;
+			case ' ':
+			case '\r':
+			case '\t':
+				  // Ignore whitespace
+				  break;
+			case '\n':
+				  line++;
+				  break;
+
+			case '"': string(); break;
+			default:
+				  if (isDigit(c))
+				  {
+					  number();
+				  }
+				  else if (isAlpha(c))
+				  {
+					  identifier();
+				  }
+				  else
+				  {
+  					  Lox.error(line, "Unexpected character.");
+				  }
+				  break;
+		}
+	}
+
+	private void identifier()
+	{
+		// While there is still numbers or characters in the stream,
+		// advance the stream
+		while (isAlphaNumeric(peek())) advance();
+
+		// Turn that part of the stream and put it into a string
+		String text = source.substring(start, current);
+		
+		// Try to turn it into a keyword if it isn't a keyword, 
+		// it is an identifier. Then, add the token to 
+		// the array of tokens
+		TokenType type = keywords.get(text);
+		if (type == null) type = IDENTIFIER;
+		addToken(type);
+	}
+
+	private void number()
+	{
+		// While there is still numbers in the stream, take them out
+		while (isDigit(peek())) advance();
+
+		// Look for a fractional part.
+		if (peek() == '.' && isDigit(peekNext()))
+		{
+			// Consume the "."
+			advance();
+
+			while (isDigit(peek())) advance();
+		}
+
+		addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+	}
+
+	private void string()
+	{
+		while (peek() != '"' && !isAtEnd())
+		{
+			if (peek() == '\n') line++;
+			advance();
+		}
+
+		if (isAtEnd())
+		{
+			Lox.error(line, "Unterminated string.");
+			return;
+		}
+
+		advance(); // The closing "
+
+		// Trim the surrounding quotes
+		String value = source.substring(start + 1, current - 1);
+		addToken(STRING, value);
+	}
+
+
+	private boolean match(char expected)
+	{
+		
+		// If at the end of the stream then there isn't another character.
+		if (isAtEnd()) return false;
+		// Actually compare the characters for inequality
+		if (source.charAt(current) != expected) return false;
+
+		// Consume the character
+		current++;
+		return true;
+	}
+
+
+	// Peek into the stream for the next character
+	private char peek()
+	{
+		
+		if (isAtEnd()) return '\0';
+		return source.charAt(current);
+	}
+
+	// Peek into the stream for the character after the next
+	private char peekNext()
+	{
+		if (current + 1 >= source.length()) return '\0';
+		return source.charAt(current + 1);
+	}
+
+	// Check if the character is alphabetical or an underscore 
+	private boolean isAlpha(char c)
+	{
+		return (c >= 'a' && c <= 'z') ||
+		       (c >= 'A' && c <= 'Z') ||
+		        c == '_';
+	}
+
+	// Check if the character is a digit
+	private boolean isDigit(char c)
+	{
+		return c >= '0' && c <= '9';
+	}
+
+	// Check if the character is either a number or a digit
+	private boolean isAlphaNumeric(char c)
+	{
+		return isAlpha(c) || isDigit(c);
+	}
+
+	// Returns the next character
+	private char advance()
+	{ 
+		return source.charAt(current++);
+	}
+
+	// add the token to the lexeme 
+	private void addToken(TokenType type)
+	{
+		addToken(type, null);
+	}
+	// Used for when tokens have values attached to them ie. Classes, variables ...
+	private void addToken(TokenType type, Object literal)
+	{
+		String text = source.substring(start, current);
+		tokens.add(new Token(type, text, literal, line));
+	}	
+}
